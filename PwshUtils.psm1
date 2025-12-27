59b2c31939dee659e1af69992630a589c3bda17d
Aelberth Cheong <aelbeth.cheong@outlook.com>
2025-12-27 16:32:50 +0700

    fix: CI workflows failed to import module

--- PwshUtils.psm1
+++ PwshUtils.psm1
@@ -4,7 +4,3 @@ Get-ChildItem $PSScriptRoot/Private/*.ps1 | ForEach-Object { . $_ }
 Set-Alias shuf    Get-ShuffledItem
 Set-Alias shuffle Get-ShuffledItem
 Set-Alias lorem   Get-LoremIpsum
-
-Export-ModuleMember `
-    -Function Get-ShuffledItem, Get-LoremIpsum, Set-DeveloperEnv `
-    -Alias shuf, shuffle, lorem
---
+++ Tests/Pester.ps1
@@ -0,0 +1,1 @@
+Import-Module "$PSScriptRoot/../PwshUtils.psd1" -Force
